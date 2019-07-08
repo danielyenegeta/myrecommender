5c2c8c279d9ca24d6a72844da9bd9246a2daa092
yenegeta <danielyenegeta35@gmail.com>
2019-07-08 19:35:06 -0400

    added templates

--- recommend/urls.py
+++ recommend/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('songview', views.songview, name='songview'),
+    path('songview/', views.songview, name='songview'),
+    path('home/<name>/', views.home, name='home'),
 ]
