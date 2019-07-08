01a3eb02713d184e444d7cab03578ccb9a670553
yenegeta <danielyenegeta35@gmail.com>
2019-07-08 18:58:26 -0400

    fixed issue with user model

--- recommend/models.py
+++ recommend/models.py
@@ -2,12 +2,12 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class CustomUser(AbstractUser):
-    name = models.CharField(max_length=30)
-    songs = models.ManyToManyField(Song)
-    def __str__(self):
-        return self.email
-
 class Song(models.Model):
     title = models.CharField(max_length=30)
     artist = models.CharField(max_length=30)
+
+class CustomUser(AbstractUser):
+    name = models.CharField(max_length=30, default="")
+    songs = models.ManyToManyField(Song)
+    def __str__(self):
+        return self.email
