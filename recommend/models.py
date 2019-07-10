92cdbe79aff716c26874fd026ea5eb4436a73abf
yenegeta <danielyenegeta35@gmail.com>
2019-07-10 19:39:24 -0400

    updated user model: added field for song recommendations

--- recommend/models.py
+++ recommend/models.py
@@ -11,5 +11,6 @@ class Song(models.Model):
 class CustomUser(AbstractUser):
     name = models.CharField(max_length=30, default="")
     songs = models.ManyToManyField(Song)
+    recommends = models.ManyToManyField(Song, related_name='custom_user_recommends')
     def __str__(self):
         return self.email
