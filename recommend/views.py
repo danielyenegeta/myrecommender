3e5c969008985a13b8456486d0977f045e736164
yenegeta <danielyenegeta35@gmail.com>
2019-07-14 12:56:46 -0400

    got recommendations working :)

--- recommend/views.py
+++ recommend/views.py
@@ -107,6 +107,79 @@ def matrix_factorization(R, P, Q, K, alpha=0.0002, beta=0.02, steps=10000):
 # nP, nQ = matrix_factorization(matrix, P, Q, K)
 # nR = numpy.dot(nP, nQ.T)
 
+def homepage(request):
+	if request.user.is_authenticated:
+		songs = request.user.songs.all()
+		recommends = request.user.recommends.all()
+		ratings = Ratings.objects.filter(person=request.user).order_by('song_id')
+		num_songs = Song.objects.latest('id').id+1
+		matrix = []
+
+		allusers = CustomUser.objects.all().order_by('id')
+		allsongs = Song.objects.all()
+		for user in allusers:
+			user_ratings = Ratings.objects.filter(person=user)
+			to_append = []
+			for i in range(1,num_songs):
+				if user_ratings.filter(song_id=i).exists():
+					to_append.append(user_ratings.get(song_id=i).rating)
+				else:
+					to_append.append(0)
+			matrix.append(to_append)
+
+		U = len(matrix)
+		V = len(matrix[0])
+		K = 7
+		P = numpy.random.rand(U,K)
+		Q = numpy.random.rand(V,K)
+		nP, nQ = matrix_factorization(matrix, P, Q, K)
+		nR = numpy.dot(nP, nQ.T)
+
+		id = request.user.id
+		for i in range(1, num_songs):
+			# if ratings.filter(song_id=i).exists():
+			# 	if ratings.get(song_id=i).rating <= 2:
+			# 		if in_newsongs:
+			# 			to_remove = request.user.newsongs.get(id=i)
+			# 			request.user.newsongs.remove(to_remove)
+			# 		break
+			# 	elif ratings.get(song_id=i).rating >= 4 and not in_newsongs:
+			# 		request.user.newsongs.add(allsongs.get(id=i))
+			# 		break
+			# if nR[id_index][i-1] >= 4:
+			# 	if not request.user.songs.filter(id=i).exists() and not in_newsongs:
+			# 		request.user.newsongs.add(allsongs.get(id=i))
+			# elif nR[id_index][i-1] < 3:
+			# 	if in_newsongs:
+			# 		to_remove = request.user.newsongs.get(id=i)
+			# 		request.user.newsongs.remove(to_remove)
+			in_songs = request.user.songs.filter(id=i).exists()
+			in_newsongs = request.user.newsongs.filter(id=i).exists()
+			if ratings.filter(song_id=i).exists():
+				user_rating = ratings.get(song_id=i).rating
+
+			if in_newsongs:
+				if user_rating < 4 or in_songs or nR[id-1][i-1] < 3:
+					to_remove = request.user.newsongs.get(id=i)
+					request.user.newsongs.remove(to_remove)
+					continue
+			elif (user_rating >= 4 or nR[id-1][i-1] >= 4) and not in_songs:
+				request.user.newsongs.add(allsongs.get(id=i))
+
+
+		recommended = request.user.newsongs.all().order_by('title')
+
+
+		context = {
+		'songs':songs,
+		'recoms':recommended,
+		'ratings':ratings,
+		'matrix':matrix,
+		'newmatrix':nR
+		}
+		return render(request, 'recommend/homepage.html', context)
+	return render(request, 'recommend/homepage.html')
+
 def index(request):
     return render(request, 'recommend/index.html')
 
@@ -127,20 +200,3 @@ def home(request, name):
 	'user':name
 	}
 	return render(request, 'recommend/home.html', context)
-
-def homepage(request):
-	if request.user.is_authenticated:
-		songs = request.user.songs.all()
-		recommends = request.user.recommends.all()
-		ratings = Ratings.objects.filter(person=request.user)
-		dontshow = []
-		for music in songs:
-			dontshow.append(Ratings.objects.filter(person=request.user, song=music))
-		context = {
-		'songs':songs,
-		'recoms':recommends,
-		'ratings':ratings,
-		'dontshow':dontshow
-		}
-		return render(request, 'recommend/homepage.html', context)
-	return render(request, 'recommend/homepage.html')
