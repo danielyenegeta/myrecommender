93a3a86c052896d5f0302e6b623887604261c416
yenegeta <danielyenegeta35@gmail.com>
2019-08-22 22:35:25 -0400

    updated settings

--- myrecommender/settings.py
+++ myrecommender/settings.py
@@ -147,7 +147,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "www", "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "frontend", "static", "frontend")
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'recommend.CustomUser'
