91aea78453b0bd5fe4173d68a2c3cbfaba663fff
yenegeta <danielyenegeta35@gmail.com>
2019-08-22 20:56:07 -0400

    changes to config

--- recommend/management/commands/createsu.py
+++ recommend/management/commands/createsu.py
@@ -1,4 +1,4 @@
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 from django.contrib.auth.models import User
 
 
@@ -7,3 +7,4 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         if not User.objects.filter(username="admin").exists():
             User.objects.create_superuser("admin", "admin@admin.com", "admin")
+            self.stdout.write(self.style.SUCCESS('Successfully created new super user'))
