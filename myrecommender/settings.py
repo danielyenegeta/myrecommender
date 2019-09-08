5b44e592122e750e98ddc86f603ad4eaa807d1a0
yenegeta <danielyenegeta35@gmail.com>
2019-09-08 17:02:41 -0400

    modified database settings

--- myrecommender/settings.py
+++ myrecommender/settings.py
@@ -121,8 +121,16 @@ WSGI_APPLICATION = 'myrecommender.wsgi.application'
 #         }
 #     }
 
-DATABASES = {}
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config(DB_NAME),
+        'USER': config(DB_USER),
+        'PASSWORD': config(DB_PASS),
+        'HOST': config(DB_HOST),
+        'PORT': 5432
+    }
+}
 
 
 # Password validation
